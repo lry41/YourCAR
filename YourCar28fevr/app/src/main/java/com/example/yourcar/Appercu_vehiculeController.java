b82f619aa84c44da4d2bbe3fb335f382a140c9da
lry41 <geoffreyleroy@orange.fr>
2022-02-28 14:13:03 +0100

    avancement des layout

---
+++ YourCar28fevr/app/src/main/java/com/example/yourcar/Appercu_vehiculeController.java
@@ -0,0 +1,16 @@
+package com.example.yourcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Appercu_vehiculeController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appercu_vehicule);
+
+    }
+
+}
