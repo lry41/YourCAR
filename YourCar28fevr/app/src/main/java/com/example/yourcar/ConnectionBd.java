36d469888933ed74459360e4d122883caf67e3ac
lry41 <geoffreyleroy@orange.fr>
2022-03-07 11:38:07 +0100

    depot 7mars

--- YourCar28fevr/app/src/main/java/com/example/yourcar/ConnectionBd.java
+++
@@ -1,6 +0,0 @@
-package com.example.yourcar;
-
-public class ConnectionBd {
-
-
-}
--- YourCar28fevr/app/src/main/java/com/example/yourcar/dto/User.java
+++
@@ -1,8 +0,0 @@
-package com.example.yourcar.dto;
-
-public class User {
-
-    private String nom, prenom, email;
-    private int num_tel;
-
-}
